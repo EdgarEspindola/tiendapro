49ea4bc55097d336700ea1ccdaf556bd3fe50860
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2025-07-11 19:45:01 -0600

    feat: Se agrega condicion para verificar stock en inventario

--- sales-service/src/main/java/com/edgarespindola/store/sales/service/impl/SaleServiceImpl.java
+++ sales-service/src/main/java/com/edgarespindola/store/sales/service/impl/SaleServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.edgarespindola.store.sales.dto.ProductInventoryResponse;
 import com.edgarespindola.store.sales.dto.SaleRequest;
 import com.edgarespindola.store.sales.dto.SaleResponse;
 import com.edgarespindola.store.sales.mapper.SaleMapper;
@@ -20,12 +22,25 @@ import lombok.RequiredArgsConstructor;
 public class SaleServiceImpl implements SaleService {
     private final SaleRepository saleRepository;
     private final SaleMapper saleMapper;
+    private final RestTemplate restTemplate;
 
     @Override
     public SaleResponse createSale(SaleRequest request) {
         Sale sale = saleMapper.toEntity(request);
         sale.setSaleDate(LocalDateTime.now());
-        sale.getItems().forEach(item -> item.setSale(sale));
+
+        for (var item : sale.getItems()) {
+            ProductInventoryResponse product = getProductFromInventory(item.getProductId());
+
+            if (product.getStock() < item.getQuantity()) {
+                // TODO: Cambiar por excepcion de dominio
+                throw new IllegalArgumentException("Stock insuficiente para el producto ID: " + item.getProductId());
+            }
+
+            item.setUnitPrice(product.getPrice());
+            item.setSale(sale);
+        }
+
         Sale saved = saleRepository.save(sale);
         return saleMapper.toDto(saved);
     }
@@ -36,5 +51,14 @@ public class SaleServiceImpl implements SaleService {
                 .stream()
                 .map(saleMapper::toDto)
                 .collect(Collectors.toList());
+    }
+
+    /* TODO: 
+        Cambiarás el localhost:8081 por el host/puerto real que uses, o incluso por el nombre de servicio si usas Eureka o Docker Compose más adelante.
+        Checar FEIGN Client
+    */    
+    private ProductInventoryResponse getProductFromInventory(Long productId) {
+        String url = "http://localhost:8081/api/products/" + productId;
+        return restTemplate.getForObject(url, ProductInventoryResponse.class);
     }
 }
